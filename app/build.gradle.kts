ef38d9f9c8e328f9fd97f2d2c3bb4f4655218a72
mkksteven240 <mkksteven240@gmail.com>
2025-09-09 22:24:38 +0530

    Profile Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.inditour"
-        minSdk = 35
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
